94bf447e205d9525fc12bb0b31604b1f03b2baf1
Debart Nathan <82568989+debart-nathan@users.noreply.github.com>
2025-09-30 16:04:00 +0200

    #4 Update normaliser_civilite.py to remove file logic

--- Lot0/normaliser_civilite.py
+++ Lot0/normaliser_civilite.py
@@ -6,12 +6,11 @@ import io
 # Normalisation de la Civilité des clients
 # ----------------------------------------------------------------------------------------------------------------
 
-def normaliser_civilite(chemin_fichier):
+def normaliser_civilite(df):
     """
     Normalise le champ 'genrecli' d'un fichier CSV selon les règles spécifiées.
     Le fichier nettoyé est sauvegardé au format Excel (*.xlsx) et une DataFrame normalisée est retournée.
     """
-    df = pd.read_csv(chemin_fichier, dtype=str, encoding="utf-8")
     
     def normaliser(civilite):
         if pd.isnull(civilite):
@@ -19,21 +18,18 @@ def normaliser_civilite(chemin_fichier):
         civilite = civilite.strip().lower()
         if civilite in ['m', 'm.']:
             return 'M.'
-        elif civilite in ['mme', 'mlle', 'mlle.', 'mlles']:
+        elif civilite in ['mme', 'mlle', 'melle.', 'melles','mlle','mlles']:
             return 'Mme'
-        elif civilite in ['m. et mme', 'm et mme']:
-            return 'M. et Mme'
+        elif civilite in ['m. et mme', 'm et mme','m. & mme', 'm & mme']:
+            return 'M. & Mme'
         else:
             return civilite
 
     if 'genrecli' in df.columns:
         df['genrecli'] = df['genrecli'].apply(normaliser)
     else:
-        raise KeyError("Le champ 'genrecli' n'existe pas dans le fichier.")
+        raise KeyError("Le champ 'genrecli' n'existe pas dans la dataframe.")
 
-    # Sauvegarder le fichier nettoyé au format Excel
-    chemin_excel = os.path.splitext(chemin_fichier)[0] + '_nettoye.xlsx'
-    df.to_excel(chemin_excel, index=False)
 
     return df
 
@@ -45,19 +41,12 @@ def normaliser_civilite(chemin_fichier):
 if __name__ == "__main__":
 
     # Création d'un DataFrame de test
-    csv_test = io.StringIO("""id,genrecli
-1,M
-2,m
-3,Mme
-4,mlle
-5,Mlles
-6,M. et Mme
-7,M et Mme
-8,
-9,Autre
-""")
-    df_test = pd.read_csv(csv_test, dtype=str)
-    df_test.to_csv("test_genrecli.csv", index=False)
+    data = {
+        'id':[1,2,3,4,5,6,7,8,9],
+        'genrecli': ['M','m','Mme','mlle','Mlles','M. et Mme','M et Mme','','Autre']
+    }
+    
+    df_test = pd.Dataframe(data)
 
-    df_result = normaliser_civilite("test_genrecli.csv")
+    df_result = normaliser_civilite(df_test)
     print(df_result)
