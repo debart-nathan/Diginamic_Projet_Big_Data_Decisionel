604a1fde5b246baad5cc8b1d5474e94c0a16da3c
Bluwen <blougd@gmail.com>
2025-09-30 15:46:05 +0200

    edit typo

--- filtre_colonne_by_value.py
+++ filtre_colonne_by_value.py
@@ -9,17 +9,17 @@ def drop_value_colonne(df, colonne, value, verbose = True):
         nb_after = (df_drop[colonne] < value).sum()
 
         # Nombre de colonnes et de lignes total avant traitement
-        print(f"Nombre de ligne  dans de le DataFrame avant transformation de la colonne datcde : {df.shape[0]}")
+        print(f"Nombre de ligne  dans de le DataFrame avant transformation de la colonne {colonne} : {df.shape[0]}")
         print()
 
-        print(f"Nombre de dates inférieurs à 2004 avant transformation : {nb_before}")
+        print(f"Nombre de dates inférieurs à {value} avant transformation : {nb_before}")
         print()
 
-        print(f"Nombre de dates inférieurs à 2004 après traitement : {nb_after}")
+        print(f"Nombre de dates inférieurs à {value} après traitement : {nb_after}")
         print()
 
         # Nombre de colonnes et de lignes total après traitement
-        print(f"Nombre de ligne  dans de le DataFrame après transformation de la colonne datcde : {df_drop.shape[0]}")
+        print(f"Nombre de ligne dans de le DataFrame après transformation de la colonne {colonne} : {df_drop.shape[0]}")
         print()
     
     return df_drop
