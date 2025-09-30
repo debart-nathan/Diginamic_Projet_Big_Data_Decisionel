cf2b766e5debb3b89d75886f0ed3eda58df4e998
debart-nathan <denathan@laposte.net>
2025-09-30 12:31:12 +0200

    #2: first analyse

--- Lot0/main.py
+++ Lot0/main.py
@@ -1,4 +1,31 @@
+import argparse
+import os
+import pandas as pd
+from analyse import analyse
+
+
+
 def main() -> int:
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="Process a file path.")
+    parser.add_argument("filepath", type=str, help="Path to the input file")
+
+
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Access the file path
+    file_path = args.filepath
+
+    df = pd.read_csv(file_path)
+
+    analyse(df)
+
+
+
+
+    
     
     return 0
 
