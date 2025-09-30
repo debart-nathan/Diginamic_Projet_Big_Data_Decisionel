4db6fd0be28ca9b546136dc86d5b9167685fe964
debart-nathan <denathan@laposte.net>
2025-09-30 15:23:44 +0200

    #7: put helper unicode_to_ascii inside clean_text_columns

--- Lot0/clean_text_columns.py
+++ Lot0/clean_text_columns.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import unicodedata
 
-def unicode_to_ascii(text):
-    if isinstance(text, str):
-        return unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('ascii')
-    return text
+
 
 def clean_text_columns(df):
+    def unicode_to_ascii(text):
+        if isinstance(text, str):
+            return unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('ascii')
+        return text
     text_cols = df.select_dtypes(include=['object', 'string']).columns
     df.loc[:, text_cols] = df.loc[:, text_cols].applymap(unicode_to_ascii)
     return df
